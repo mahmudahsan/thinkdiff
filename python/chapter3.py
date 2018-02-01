5f744cff50107b330cfb87ad8f0f9c86f9e2be8c
Mahmud Ahsan <mahmud@thinkdiff.net>
2018-02-01 10:55:53 +0800

    chapter 3 code added

---
+++ python/chapter3.py
@@ -0,0 +1,99 @@
+# author Mahmud Ahsan
+
+# Statement
+# প্রোগ্রামিংয়োর ভাষায় কোডের ক্ষুদ্রতম অংশ যা পাইথন ইনটারপেটার এক্সকিউট করতে পারে সেটাই স্টেটমেন্ট
+
+print ("Hello World")
+x = 2
+x = 2 + 1
+
+
+# Expression
+# ভ‍্যালু, ভ‍্যারিয়েবল এবং অপারেটর এর কম্বিনেশন কেই এক্সপ্রেশন বলে
+x = 17
+y = x + 2 
+
+
+# String
+
+# Accessing string character
+title = "Python Course"
+print ( title[0], title[1], title[-1], title[-2])
+
+# String operation
+name = 'jonathon swift'
+print ( name.title() )
+print ( name.upper() )
+print ( name.lower() )
+
+# String Concatenation
+
+first_name = "Steve"
+last_name  = "Jobs"
+name = first_name + ' ' + last_name
+print ( name )
+
+print ( first_name + ' ' + last_name )
+
+# Newline
+print (first_name + "\n" + last_name)
+
+# Whitespace
+name = "        Mr. X        "
+print('_' + name + '_')
+print('_' + name.lstrip() + '_')
+print('_' + name.rstrip() + '_')
+print('_' + name.strip() + '_')
+print('_' + name.lstrip().rstrip() + '_')
+
+# Printing Single and Double Quote
+shop_name = "Rahim's Shop"
+print(shop_name)
+
+shop_name = 'Rahim"s Shop'
+print(shop_name)
+
+shop_name = 'Rahim\'s Shop'
+print(shop_name)
+
+# Matching text at star and at the end
+filename = 'bigdata.txt'
+print ( filename.endswith('.txt') )
+print ( filename.startswith('bi') )
+
+# Find word in sentence
+sentence = "A quick brown fox jumps over the lazy dog"
+print ( sentence.find('fox') )
+print ( sentence.find('foxs') ) # -1 the valu not found
+
+# Replace text
+sentence = "A quick brown fox jumps over the lazy dog"
+sentence = sentence.replace('fox', 'tiger')
+print ( sentence )
+
+sentence = "A quick brown fox jumps over the lazy fox"
+sentence = sentence.replace('fox', 'tiger')
+print ( sentence )
+
+# Print separator
+x = 'Dhaka'
+y = 'Bogra'
+z = 'Comilla'
+
+print (x + '| ' + y + '| ' + z)
+print (x, y, z, sep='| ')
+
+# String interpolation
+person = '{name}\'s age is {age}'
+print(person.format(name='Bill', age=55))
+print(person.format(name='Steve', age=50))
+
+person = '%s\'s age is %d'
+print(person % ('Bill', 55) )
+
+# String slice
+name = "Taylor Swift"
+print (name[0: 6])
+print (name[:6])
+print (name[7:])
+print (name[7:-1])
