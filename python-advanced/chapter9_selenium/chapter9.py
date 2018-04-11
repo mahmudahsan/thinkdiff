b59374ad9601d73ef2658d93ffeeebb758ea25a4
Mahmud Ahsan <mahmud@thinkdiff.net>
2018-04-11 16:29:13 +0600

    chapter 9 updated

--- python-advanced/chapter9_selenium/chapter9.py
+++ python-advanced/chapter9_selenium/chapter9.py
@@ -19,19 +19,27 @@ def main():
     chrome_options.add_argument("--headless")
     chrome_options.add_argument("--window-size=1024x1400")
 
-    # download chrome driver https://sites.google.com/a/chromium.org/chromedriver/downloads and put exe file in same directory
-    
+    # download Chrome Webdriver  
+    # https://sites.google.com/a/chromium.org/chromedriver/download
+    # put driver executable file in the script directory
     chrome_driver = os.path.join(os.getcwd(), "chromedriver")
+
     driver = webdriver.Chrome(chrome_options=chrome_options, executable_path=chrome_driver)
 
     driver.get("https://github.com")
-    assert "GitHub".lower() in driver.title.lower() # testing purpose
+    assert "GitHub".lower() in driver.title.lower()
+    
+    # scrap info
+    h1_elem = driver.find_element_by_tag_name("h1")
+    print(h1_elem.text)
 
+    # fill and submit form
     elem = driver.find_element_by_name("q")
     elem.clear()
     elem.send_keys("python")
     elem.send_keys(Keys.RETURN)
 
+    # screenshot capture
     driver.get_screenshot_as_file("python-github.png")
     driver.close()
 
