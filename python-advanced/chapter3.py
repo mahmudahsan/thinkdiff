26d8bd1404fd7742d2a4a9c0622a24bbe2e284d2
Mahmud Ahsan <mahmud@thinkdiff.net>
2018-08-18 11:23:21 +0800

    header comments updated

--- python-advanced/chapter3.py
+++ python-advanced/chapter3.py
@@ -1,10 +1,8 @@
 # author: Mahmud Ahsan
-# github: https://github.com/mahmudahsan
+# code: https://github.com/mahmudahsan/thinkdiff
 # blog: http://thinkdiff.net
-# Web: http://pythonbangla.com
-# youtube: https://www.youtube.com/c/banglaprogramming
-# License: MIT License
-# https://github.com/mahmudahsan/thinkdiff/blob/master/LICENSE 
+# http://pythonbangla.com
+# MIT License
 
 # --------------------------
 #      Regular Expression
---
+++ python-advanced/list_slice.py
@@ -0,0 +1,9 @@
+# author: Mahmud Ahsan
+# code: https://github.com/mahmudahsan/thinkdiff
+# blog: http://thinkdiff.net
+# http://pythonbangla.com
+# MIT License
+
+# --------------------------
+#  List Slicing
+# --------------------------
