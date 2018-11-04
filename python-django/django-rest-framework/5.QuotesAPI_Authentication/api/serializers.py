f52c1f0efdb8851ac753995eda06a566b646ddbf
Mahmud Ahsan <mahmud@thinkdiff.net>
2018-11-05 01:46:21 +0800

    authentication code added

---
+++ python-django/django-rest-framework/5.QuotesAPI_Authentication/api/serializers.py
@@ -0,0 +1,18 @@
+from rest_framework import serializers
+
+from quotes.models import Quote
+from quotes.models import QuoteCategory 
+
+class QuoteSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Quote 
+        # ('__all__')  for all fields
+        # ['quote', 'author']
+        fields = ('__all__')
+
+class QuoteCategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = QuoteCategory 
+        # ('__all__')  for all fields
+        # ['title']
+        fields = ('__all__')
---
+++ python-django/django-rest-framework/5.QuotesAPI_Authentication/quotes/views.py
@@ -0,0 +1,14 @@
+from django.views.generic import ListView
+
+from . models import Quote
+from . models import QuoteCategory
+
+class HomeView(ListView):
+    template_name = "home.html"
+    model = Quote
+
+    def get_queryset(self):
+        query_set = super().get_queryset()
+        return query_set.select_related('quote_category')
+
+
