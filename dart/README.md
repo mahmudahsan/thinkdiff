b9691b9c3c48b9379e0bab852df33c86a6cc3674
Mahmud Ahsan <mahmud@thinkdiff.net>
2019-06-13 22:14:06 +0800

    Another async/await example added for dart program

--- dart/README.md
+++ dart/README.md
@@ -1463,3 +1463,36 @@ Good
 ```
 
 > We can also use `try..catch` block to catch error for asynchronous operation.
+
+**Another Example**
+
+When `task3()` depends on `task2()`
+
+```dart
+void task1() {
+  print('Task 1 Done.');
+}
+
+Future <String> task2() async {
+  Duration duration = Duration(seconds: 2);
+  
+  String result;
+
+  await Future.delayed(duration, () {
+    print('Task 2 Done.');
+    result = ' Task 2 Data';
+  });
+
+  return result;
+}
+
+void task3(String result) {
+  print('Task 3 Done. $result');
+}
+
+void main() async {
+  task1();
+  String result = await task2();
+  task3(result);
+}
+```
--- dart/playground.dart
+++ dart/playground.dart
@@ -1,12 +1,26 @@
-Future delayedPrint(int seconds, String msg) {
-  final duration = Duration(seconds: seconds);
-  return Future.delayed(duration).then((value) => msg);
+void task1() {
+  print('Task 1 Done.');
 }
 
-main() {
-  print('Life');
-  delayedPrint(2, "Is").then((status) {
-    print(status);
-  }).catchError((err) => print(err));
-  print('Good');
+Future <String> task2() async {
+  Duration duration = Duration(seconds: 2);
+  
+  String result;
+
+  await Future.delayed(duration, () {
+    print('Task 2 Done.');
+    result = ' Task 2 Data';
+  });
+
+  return result;
+}
+
+void task3(String result) {
+  print('Task 3 Done. $result');
+}
+
+void main() async {
+  task1();
+  String result = await task2();
+  task3(result);
 }
